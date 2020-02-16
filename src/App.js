c39e04af25c036cf14a86482a3e38e4022a7aa2b
Beta-23 <alfred.dominguez@gmail.com>
2020-02-15 18:45:52 -0800

    Fix and link app style component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import UserProfile from './components/user-profile/user-profile.component';
 import UserList from './components/user-list/user-list.component';
 
+import './App.scss';
 
 function App() {
   return (
